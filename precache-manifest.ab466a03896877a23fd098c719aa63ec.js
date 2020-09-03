1056e464dd18b8ecddae9bd9e559297e798eddaa
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-09-03 15:58:54 +0300

    Updates

---
+++ precache-manifest.ab466a03896877a23fd098c719aa63ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116a078e161dfbdb2dd7bda507b411fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6740ffbff83e9f4d122d",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "affb88752fff2e6ff9ac",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "6740ffbff83e9f4d122d",
+    "url": "./static/js/2.834b62b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.834b62b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affb88752fff2e6ff9ac",
+    "url": "./static/js/main.3f3a5f03.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
